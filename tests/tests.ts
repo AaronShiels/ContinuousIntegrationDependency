b69e7732585c789f2aedcbac866bf086fb505fe0
Aaron Shiels <aaron.shiels@outlook.com>
2021-11-17 17:01:39 -0800

    MP-69 Demonstrate workflows

--- tests/tests.ts
+++ tests/tests.ts
@@ -2,4 +2,9 @@ import { foo } from "../continuousintegrationdependency";
 
 test("it works", () => {
 	const result = foo();
+	expect(true).toBeTruthy();
+});
+
+test("it no works", () => {
+	expect(true).toBeFalsy();
 });
